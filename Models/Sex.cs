34f23e551affd3e7b73abbe17f7e7212d873ae16
Your Name <you@example.com>
2022-11-26 18:55:15 +0000

    hello :wq :wq

---
+++ Models/Sex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace disneyworld.Models;
+
+public partial class Sex
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? ArName { get; set; }
+
+    public virtual ICollection<Person> People { get; } = new List<Person>();
+
+
+}
